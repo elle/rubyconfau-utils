9d7b81ffc32984d63b64feae4e7ef7de92b86864
Keith Pitty <keith@keithpitty.com>
2013-11-16 15:21:46 +1100

    Cleaned up.

--- extract_proposals.rb
+++ extract_proposals.rb
@@ -5,8 +5,6 @@ require 'csv'
 
 puts 'Getting ready to extract conference proposals...'
 
-puts 'Accessing repo...'
-
 pull_requests = Github::PullRequests.new.list user: 'rubyaustralia', repo: 'rubyconfau-2014-cfp', per_page: 100
 pull_requests = pull_requests.sort_by { |pr| pr["number"] }
 
@@ -14,8 +12,10 @@ puts "Found #{pull_requests.length} pull requests..."
 
 CSV.open("extract.csv", "wb", headers: ['number', 'title', 'body']) do |csv|
   pull_requests.each do |pr|
-    puts "About to extract data from pull request ##{pr['number']}..."
     csv << [ pr['number'], pr['title'], pr['body']]
+    print '.'
   end
 end
 
+puts ''
+puts "Done."
